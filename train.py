94ad5b22846eb055d7d421e6434d354f59dabdb0
dal <dal.noguer@advertima.com>
2020-11-27 18:42:24 +0100

    Refactored

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 @click.command()
 @click.argument('experiment_path', default=".", type=click.Path(exists=True))
 def train(experiment_path):
+
     assert os.path.exists(
         os.path.join(experiment_path, "experiment.yaml")
     ), "No experiment configuration was found, please create an experiment.yaml"
@@ -21,17 +22,18 @@ def train(experiment_path):
         configuration_dict = yaml.load(config_file, Loader=yaml.Loader)
 
     model = TextClassificationModel(configuration_dict)
-    tb_logger = pl_loggers.TensorBoardLogger('logs/')
+    tb_logger = pl_loggers.TensorBoardLogger(
+        configuration_dict['Training']['log_dir'])
 
     assert not os.path.exists(configuration_dict['Training']
                               ['snapshot_dir']), "Experiment already exists."
 
-    checkpoint = ModelCheckpoint(filepath=os.path.join(
-        configuration_dict['Training']['snapshot_dir'],
-        'best_model_{epoch:02d}-{val_loss:.2f}'),
-                                 verbose=True,
-                                 monitor='val_loss',
-                                 mode='min')
+    checkpoint = ModelCheckpoint(
+        filepath=os.path.join(configuration_dict['Training']['snapshot_dir'],
+                              'best_model_{epoch:02d}-{val_loss:.2f}'),
+        verbose=True,
+        monitor=configuration_dict['Training']['snapshot_selection_scheme'],
+        mode='min')
 
     trainer = Trainer(logger=tb_logger,
                       checkpoint_callback=checkpoint,
